de5717220ce1dba883a3a67f5da5d5c786d23539
baitutanglj <2919588049@qq.com>
2023-04-14 15:41:33 +0800

    del pretrain_model

--- pretrain_model_lstm/a.py
+++
@@ -1,9 +0,0 @@
-c = 0
-for el, value in self.store.items():
-    random.shuffle(value)
-    print(len(self.store[el]))
-    a = self.store[el]
-    print(len(a))
-    c += 1
-    if c > 10:
-        break
